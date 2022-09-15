3dbf0cdae4e88f835635ba98527c99e3183e7d10
Hemang Sharma <59247602+hemangsharma@users.noreply.github.com>
2022-09-15 12:06:08 +0530

    Initial Commit

---
+++ CatchInAR/CatchInARApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchInARApp.swift
+//  CatchInAR
+//
+//  Created by Hemang Sharma on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchInARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
